2b12cd0d9d6630c0b394d15ea45e882cd5528af0
Svetlana Panina <paninasi@inbox.ru>
2023-08-22 10:09:29 +0300

    Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 42:
+// Напишите программу, которая
+// будет преобразовывать десятичное число в двоичное.
+// 46 –> 101110
+// 13 –> 1101
+// 2  –> 10
+
+int ConvertNumberDecimalToBinary(int num)
+{
+    int convertNum = 0;
+    int i = 1;
+    while (num > 0)
+    {
+        convertNum += num % 2 * i;
+        num /= 2;
+        i *= 10;
+    }
+    return convertNum;
+}
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = ConvertNumberDecimalToBinary(number);
+
+Console.WriteLine($"Десятичное число {number} в двоичном формате будет: {result}");
